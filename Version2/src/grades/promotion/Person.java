3a4162fe42e69540101a333a5a1208b77437e6c8
mariagrs <43874158+mariagrs@users.noreply.github.com>
2019-05-05 15:47:01 +0200

    Update Person.java

--- Version2/src/grades/promotion/Person.java
+++ Version2/src/grades/promotion/Person.java
@@ -1,12 +1,6 @@
 package grades.promotion;
 
-/*
- *
- * Q1) : Chaque professeur et chaque élève ont comme propriétés communes le prénom et le
- * nom. On a donc crée une classe Person qui représentera un individu quelconque, dont les classes Student et Teacher
- * hériteront; le concept associé est donc l'héritage.
- *
- */
+
 
 public class Person {
 
